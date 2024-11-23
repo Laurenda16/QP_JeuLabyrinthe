f662ac44e095c68816f01291f7418457abc221e0
Laurenda Edorh <laurenda98@gmail.com>
2024-11-23 11:28:23 +0100

    envois de mes modification

--- QP_JeuLabyrinthe/robot.h
+++ QP_JeuLabyrinthe/robot.h
@@ -1,15 +1,33 @@
 #ifndef ROBOT_H
 #define ROBOT_H
+#include <memory>
+
+#include "point.h"
+#include "observateurRobot.h"
+
 
 
 class robot
 {
     public:
         robot();
+        bool obstacleEstDetecte() const;
+        int avanceUneCase();
+        void tounerAGauche() ;
+        void tounerADroite() ;
+        void notifierObservation() const ;
+        void enregistrerObservateur() ;
 
-    protected:
+///cardinal direction
+  enum { east=0, north=90, west=180, south=270 };
+
 
     private:
+       geom::point d_pos;
+     //std::vector<std::unique_ptr<observateurRobot> d_observateurs;
+       // direction
 };
 
 #endif // ROBOT_H
+
+
