89654cd799bf5454bee28d25075a592492d864d0
Binta <fatoumata-binta.diallo@uha.fr>
2024-12-20 00:38:41 +0000

    Ajout de la méthode de la classe observateur

--- observateur.h
+++ observateur.h
@@ -1,7 +1,14 @@
-
+#ifndef OBSERVATEUR_H
+#define OBSERVATEUR_H
 
 class observateur
 {
 public:
+    virtual void notifie(const robot& r) const = 0;
+    virtual ~observateur() = default;
 
 };
+
+
+#endif // OBSERVATEUR_H
+
