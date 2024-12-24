2007bfa192e52a7a4651d821a168122f1dad831f
Binta <fatoumata-binta.diallo@uha.fr>
2024-12-24 01:23:17 +0000

    Ajout de quelques remaniements

--- QP_JeuLabyrinthe/algorithme.cpp
+++ QP_JeuLabyrinthe/algorithme.cpp
@@ -7,22 +7,20 @@
 
 void algoMainDroite(robot& r) {
     while (true) {
-        // Si le robot détecte un obstacle à droite, avancer ou tourner à gauche
-        r.tounerADroite(); // Tourner à droite pour vérifier s'il y a un mur
+        r.tounerADroite();
         if (!r.detecterObstacle()) {
-            r.avanceUneCase(); // Si pas d'obstacle, avancer
+            r.avanceUneCase();
         } else {
-            r.tounerAGauche(); // Sinon, retourner à la direction précédente
+            r.tounerAGauche();
             if (r.detecterObstacle()) {
-                r.tounerAGauche(); // Si un mur est devant, tourner à gauche
+                r.tounerAGauche();
             } else {
-                r.avanceUneCase(); // Sinon, avancer
+                r.avanceUneCase();
             }
         }
-        r.notifierObservation(); // Notifier les observateurs du déplacement
-        // Condition d'arrêt (exemple : si le robot atteint une position cible)
+        r.notifierObservation();
         if (r.positionRobot().x == caseArrivee().x && r.positionRobot().y == caseArrivee().y)) {
-            break; // Fin de l'algorithme si le robot revient à (0, 0)
+            break;
         }
     }
 }
